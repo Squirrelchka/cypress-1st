33d7a264ded03c2a88ccc7df3c730bf55a57c277
KrisGromova <levkindom@gmail.com>
2023-12-05 22:29:01 +0300

    hw_5.2.1 update

--- cypress/e2e/test_1.cy.js
+++ cypress/e2e/test_1.cy.js
@@ -5,7 +5,6 @@ describe('home page', () => {
         cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/')
   })
     it('page loading check', () => {
-      cy.get('.table-responsive div').should('have.length', 21)
+      cy.get('.table-responsive div').should('have.length', 8)
            })
     })
-    
