167a539ffbac9fc350f7bdaf4be59a51a7d189f3
KrisGromova <levkindom@gmail.com>
2023-12-11 14:52:16 +0300

    hw_5.2.2 update cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
     projectId: "nnsfu4",
     e2e: {
+      baseUrl: 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/',
+      Username: "gromova_student",
+      Password: "Qwerty123!",
+      video: true, 
       setupNodeEvents(on, config) {
         // implement node event listeners here
       }
-    },
+    }, 
 });
 
